ea9fffc42588a4f6c8d78468c09def1d236eef6a
mohit sharma <mohit.shar1005@gmail.com>
2019-03-15 01:52:47 +0530

    js oloo model problem solution

--- playground/practise-js/ex4/ex4-oloo.js
+++ playground/practise-js/ex4/ex4-oloo.js
@@ -1,9 +1,9 @@
-var Widget = {
+var Widget={
     init: function(width, height) {
-        this.width = width || 50;
-        this.height = height || 50;
-        this.$elem = null;
-    },
+            this.width = width || 50;
+            this.height = height || 50;
+            this.$elem = null;
+        },
     render: function ($where) {
         if (this.$elem) {
             this.$elem.css({
@@ -11,29 +11,39 @@ var Widget = {
                 height: this.height + "px"
             }).appendTo($where);
         }
-    }
+    }    
 }
+
 var Button = Object.create(Widget);
-Button.initButton = function (label, width, height){
+Button.init = function (label, height, width) {
     this.label = label;
-    //delegated call
-    this.init(height, width);
+    this.height = height;
+    this.width = width;
     this.$elem = $("<button>").text(this.label);
 }
 Button.renderButton = function ($where) {
-    //delegated call
     this.render($where);
     this.$elem.bind('click', this.onClick.bind(this));
-};
+}
 Button.onClick = function () {
-    console.log(this.label);
-};
+    console.log(this.label + ' clicked');
+}
+/*
+Button -> render = function($where) {
+	// call the parent render()
+	// add a click handler -> onClick
+}
+
+Button -> onClick = function(evt) {
+	console.log("...");
+}
+*/
 $(document).ready(function () {
     var $body = $(document.body);
     var btn1 = Object.create(Button);
-    var btn2 = Object.create(Button);
-    btn1.initButton("Button 1");
-    btn2.initButton("Button 2", 150, 150);
+    btn1.init('btn1', 50, 100);
     btn1.renderButton($body);
+    var btn2 = Object.create(Button);
+    btn2.init('btn2', 100, 200);
     btn2.renderButton($body);
 });
