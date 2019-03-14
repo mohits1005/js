60b2140c8a882adda590ec4107f689f0e77981ec
mohit sharma <mohit.shar1005@gmail.com>
2019-03-15 01:31:42 +0530

    js classic inheritance solution

--- playground/practise-js/ex4/ex4.js
+++ playground/practise-js/ex4/ex4.js
@@ -1,9 +1,10 @@
-function Widget(width,height) {
+function Widget(width, height) {
 	this.width = width || 50;
 	this.height = height || 50;
 	this.$elem = null;
 }
-Widget.prototype.render = function($where){
+
+Widget.prototype.render = function ($where) {
 	if (this.$elem) {
 		this.$elem.css({
 			width: this.width + "px",
@@ -11,22 +12,36 @@ Widget.prototype.render = function($where){
 		}).appendTo($where);
 	}
 };
-function Button(label, width, height) {
+
+function Button(label, height, width) {
 	this.label = label;
-	Widget.call(this, height, width);
+	this.height = height;
+	this.width = width;	
 	this.$elem = $("<button>").text(this.label);
 }
-Button.prototype.renderButton = function ($where) {
-	Widget.prototype.render.call(this, $where);
+Button.prototype = Object.create(Widget.prototype);
+Button.prototype.renderButton = function ($where){
+	this.render($where);
 	this.$elem.bind('click', this.onClick.bind(this));
-};
+}
 Button.prototype.onClick = function () {
-	console.log(this.label);
-};
+	console.log(this.label+' clicked');
+}
+/*
+Button -> render = function($where) {
+	// call the parent render()
+	// add a click handler -> onClick
+}
+
+Button -> onClick = function(evt) {
+	console.log("...");
+}
+*/
 $(document).ready(function(){
 	var $body = $(document.body);
-	var btn1 = new Button("Button 1");
-	var btn2 = new Button("Button 2", 150,150);
+	var btn1 = new Button('btn1', 50, 100);
+	var btn2 = new Button('btn2', 100, 200);
+
 	btn1.renderButton($body);
 	btn2.renderButton($body);
 });
